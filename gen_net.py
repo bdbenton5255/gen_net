8007bf1351a7d6256de07bbff878c9c8b6209469
Brandon Benton <brandon.benton.5255@gmail.com>
2025-06-11 03:31:27 -0500

    mean squared error loss function

--- gen_net.py
+++ gen_net.py
@@ -1,5 +1,13 @@
 import numpy as np
 
+def mse_loss(y_true, y_pred):
+    return ((y_true - y_pred) ** 2).mean()
+
+y_true = np.array([1, 0, 0, 1])
+y_pred = np.array([0, 0, 0, 0])
+
+print(mse_loss(y_true, y_pred))
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
